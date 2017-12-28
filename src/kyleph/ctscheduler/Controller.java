9584b7f066e5f8b6e96f6a83ed0397d0819abfd6
Kyle Hinkel <kylehinkel@gmail.com>
2017-12-28 15:31:04 -0500

    Testing, gotta go

--- src/kyleph/ctscheduler/Controller.java
+++ src/kyleph/ctscheduler/Controller.java
@@ -1,8 +1,23 @@
 package kyleph.ctscheduler;
 
+import javafx.application.HostServices;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+
+import java.io.File;
+import java.net.MalformedURLException;
 
 public class Controller {
+
+    @FXML
+    TableView table;
+
+    HostServices hostServices;
+    File scheduleExcelFile;
 
     /*
 
@@ -11,7 +26,19 @@ public class Controller {
      */
     @FXML
     protected void mnuOpenAddEmployeeForm() {
+        ObservableList<ObservableList<String>> superList = FXCollections.observableArrayList();
+        ObservableList<String> kiddieList = FXCollections.observableArrayList();
 
+        for(int i = 0; i < 25; ++i) {
+            TableColumn tempColumn = new TableColumn("" + i);
+            for(int j = 0; j < 25; j++) {
+                kiddieList.add("" + j);
+            }
+            table.getColumns().add(tempColumn);
+            superList.add(kiddieList);
+            kiddieList.clear();
+        }
+        table.setItems(superList);
     }
 
     @FXML
@@ -25,7 +52,7 @@ public class Controller {
     }
 
     @FXML
-    protected void mnuOpenViewEmployeesForm() {
+    protected void mnuOpenViewEmployeesForm() throws MalformedURLException {
 
     }
 
@@ -68,6 +95,17 @@ public class Controller {
     @FXML
     protected void buttonSaveSchedule() {
 
+    }
+
+    @FXML
+    protected void buttonOpenScheduleInExcel() throws MalformedURLException {
+        hostServices.showDocument(scheduleExcelFile.toURI().toURL().toExternalForm());
+    }
+
+    // Receives the HostServices object associated with the application.
+    // This is called from Main in the start method.
+    protected void setHostServices(HostServices hostServices) {
+        this.hostServices = hostServices;
     }
 
 }
--- src/kyleph/ctscheduler/Main.java
+++ src/kyleph/ctscheduler/Main.java
@@ -2,8 +2,11 @@ package kyleph.ctscheduler;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.stage.Stage;
@@ -12,7 +15,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent form = FXMLLoader.load(getClass().getResource("mainForm.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainForm.fxml"));
+        Parent form = loader.load();
 
         // Set the JavaFX form to accept drag and drop events.
         // This can't be done on the FXML form for some reason.
@@ -25,6 +29,12 @@ public class Main extends Application {
                 event.consume();
             }
         });
+
+        // Send the HostServices for this application to the
+        // controller class. HostServices is needed to preview
+        // excel files and is part of the Application class.
+        Controller controller = loader.getController();
+        controller.setHostServices(getHostServices());
 
         Scene scene = new Scene(form, 600, 600);
         primaryStage.setTitle("ConTav Schedule Builder");
